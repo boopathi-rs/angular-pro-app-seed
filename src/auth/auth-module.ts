f75a47f93768d16c9f492e36a314410e90dd0bb0
Boopathi Ramasamy <boopathi.rs@gmail.com>
2022-01-03 00:10:56 +0000

    got error now - Uncaught Error: Unexpected value 'AuthService' imported by the module 'SharedModule'. Please add a @NgModule annotation.     at syntaxError (compiler.es5.js:1690)

---
+++ src/auth/auth-module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+
+// third-party modules
+
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+// Shared modules
+import { SharedModule } from './shared/shared.module';
+
+export const ROUTES: Routes = [
+    {
+        path: 'auth',
+        children: [
+            { path: '', pathMatch: 'full', redirectTo: 'login' },
+            { path: 'login', loadChildren: './login/login.module#LoginModule' },
+            { path: 'register', loadChildren: './register/register.module#RegisterModule' }
+        ]
+    }
+]
+
+export const firebaseConfig: FirebaseAppConfig = {
+    apiKey: "AIzaSyCaBs3CmRguAWEGGWhk9PUfCD9frruteZ8",
+    authDomain: "fitness-app-93c32.firebaseapp.com",
+    databaseURL: "https://fitness-app-93c32-default-rtdb.firebaseio.com",
+    projectId: "fitness-app-93c32",
+    storageBucket: "fitness-app-93c32.appspot.com",
+    messagingSenderId: "379679711229"    
+  };
+
+@NgModule({
+    imports: [
+        CommonModule,
+        RouterModule.forChild(ROUTES),
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFireAuthModule,
+        AngularFireDatabaseModule,
+        SharedModule
+    ],
+    declarations: [],
+    providers: []
+})
+export class AuthModule {
+
+}
